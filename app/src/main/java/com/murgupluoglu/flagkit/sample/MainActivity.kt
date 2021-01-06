8857e826a9c4d1335ccc68a0d06aa4dbf70d2e2b
Mustafa Urgupluoglu <mustafa.urgupluoglu@applogist.com>
2021-01-06 11:11:47 +0300

    init

---
+++ app/src/main/java/com/murgupluoglu/flagkit/sample/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.murgupluoglu.flagkit.sample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.github.nitrico.lastadapter.LastAdapter
+import com.github.nitrico.lastadapter.Type
+import com.murgupluoglu.flagkit.FlagKit
+import com.murgupluoglu.flagkit.sample.databinding.ItemFlagBinding
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val flags = arrayListOf<FlagModel>()
+        FlagKit.getAllAvailableCodes().forEach {
+            //flags.add(FlagModel(it, FlagKit.getResId(this, it)))
+        }
+
+        val drawable = FlagKit.getDrawable(this, "not_exist")
+
+        for (locale in Locale.getISOCountries()) {
+            flags.add(FlagModel(locale, FlagKit.getResId(this, locale)))
+        }
+
+        Log.i("flags", flags.toString())
+        Log.i("flags", flags.size.toString())
+
+
+        LastAdapter(flags, BR.item)
+                .map<FlagModel>(
+                        Type<ItemFlagBinding>(R.layout.item_flag)
+                                .onBind {
+                                    val item = it.binding.item!!
+                                    it.binding.apply {
+                                        flagImageView.setImageResource(item.resourceId)
+                                        nameTextView.text = item.name
+                                    }
+                                }
+
+                )
+                .into(findViewById(R.id.recyclerView))
+    }
+}
