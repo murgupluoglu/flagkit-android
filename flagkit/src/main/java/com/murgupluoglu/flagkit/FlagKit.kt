1f26f0875907fd2bea4f86bd4dd2e6b3db6bcd10
Gerard Coll <webserveis@gmail.com>
2022-03-11 17:41:17 +0100

    countryCodeToEmojiFlag
    get emogi flag

--- flagkit/src/main/java/com/murgupluoglu/flagkit/FlagKit.kt
+++ flagkit/src/main/java/com/murgupluoglu/flagkit/FlagKit.kt
@@ -295,4 +295,18 @@ object FlagKit {
             "zw"
         )
     }
+        
+    fun countryCodeToEmojiFlag(countryCode: String): String {
+        return countryCode
+            .uppercase(Locale.US)
+            .map { char ->
+                Character.codePointAt("$char", 0) - 0x41 + 0x1F1E6
+            }
+            .map { codePoint ->
+                Character.toChars(codePoint)
+            }
+            .joinToString("") { charArray ->
+                String(charArray)
+            }
+    }
 }
